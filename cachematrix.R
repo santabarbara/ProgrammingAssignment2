fcaf9ff85e412501e4c842607c61d4e1b963c14c
unknown <ruiz-i@IMK-IFU-L2914.gap.fzk.de>
2014-04-17 20:41:38 +0200

    Added the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+# In this exercise we use the `<<-` operator which can be used to assign a value
+# to an object in an environment that is different from the current environment.
+# Below are two functions that are used to create a special object that stores a
+# numeric matrix and cache's its inverse.
 
-## Write a short comment describing this function
+
+# The first function, `makeCacheMatrix` creates a special "matrix", which is 
+# really a list containing a function to
+# * 1.  set the value of the matrix 
+# * 2.  get the value of the matrix 
+# * 3.  set the value of the inverse 
+# * 4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -21,7 +29,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of the special "matrix"
+# created with the above function. However, it first checks to see if the
+# inverse has already been calculated. If so, it `get`s the inverse from the
+# cache and skips the computation. Otherwise, it calculates the inverse of
+# the data and sets the value of the inverse in the cache via the `setinverse`
+# function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
